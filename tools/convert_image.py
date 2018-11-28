63d5d269aa179cfe07373b19e95fe56f3b1cb255
Mike <malphas@subdimension.co.uk>
2018-11-28 11:41:16 +0000

    make convert image cleverer

--- tools/convert_image.py
+++ tools/convert_image.py
@@ -1,13 +1,31 @@
+from argparse import ArgumentParser
 from PIL import Image
 
-IMG = "image.png"
+parser = ArgumentParser()
 
-im = Image.open(IMG)
+parser = ArgumentParser(description='Convert monochrome images into C arrays of bytes')
+parser.add_argument('input_image', help='the image to convert')
+                    
+parser.add_argument('-o', 
+                    dest='output_format', 
+                    choices=('t', 'i'),
+                    default='i',
+                    help='output as 8x8 tiles(=t) or raw image(=i) (default)')
+                    
+parser.add_argument('-f', dest='output_filename', default='image.txt', help='output filename')
+
+args = parser.parse_args()
+
+im = Image.open(args.input_image)
 pixels = list(im.getdata())
 
 width = im.size[0]
 height = im.size[1]
 im.close()
+
+if width % 8 > 0 or height % 8 > 0:
+    print("Image dimensions not divisible by 8.\n\nThis is currently unsupported.")
+    exit(1)
 
 image = []
 
@@ -20,12 +38,21 @@ for y in range(height // 8):
                 b = b | (1 << i)
         image.append(b)
 
-f = open('image.txt', 'w')
-for y in range(height//8):
-    line = ''
-    for x in range(width):
-        line = line + "{:#04x}, ".format(image.pop(0))
+f = open(args.output_filename, 'w')
+
+if args.output_format == 't': 
+    while(len(image) > 0):
+        block = image[:8]
+        image = image[8:]
+        f.write("{0:#04x}, {1:#04x}, {2:#04x}, {3:#04x}, {4:#04x}, {5:#04x}, {6:#04x}, {7:#04x}, \n".format(*block))
+    #print("not implemented")
         
-    f.write(line + "\n")
+else:
+    for y in range(height // 8):
+        line = ''
+        for x in range(width):
+            line = line + "{:#04x}, ".format(image.pop(0))
+            
+        f.write(line + "\n")
         
 f.close()
